68a2be81991e3e1e71f5c87e4188c3b38c2469e3
Jeff Cogswell <jeffcogs@gmail.com>
2022-11-17 11:50:43 -0500

    Added service code

--- BeveragesFront/src/app/drink.service.ts
+++ BeveragesFront/src/app/drink.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Drink } from './drink';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DrinkService {
 
-  constructor() { }
+	getAll(cb: any) {
+		// https://localhost:7147/Drink
+		this.http.get<Drink[]>('https://localhost:7147/Drink').subscribe(cb);
+	}
+
+	getOne(cb: any, id: number) {
+		// Typo! Take out the [].
+		this.http.get<Drink>(`https://localhost:7147/Drink/${id}`).subscribe(cb);
+	}
+
+	add(cb: any, emp: Drink) {
+		this.http.post<Drink>('https://localhost:7147/Drink', emp).subscribe(cb);
+	}
+
+	delete(cb: any, id: number) {
+		this.http.delete(`https://localhost:7147/Drink/${id}`).subscribe(cb);
+	}
+
+	update(cb: any, emp: Drink) {
+		this.http.put('https://localhost:7147/Drink', emp).subscribe(cb);
+	}
+
+	constructor(private http: HttpClient) { }
 }
