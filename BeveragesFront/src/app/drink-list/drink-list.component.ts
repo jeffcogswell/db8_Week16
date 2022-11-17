5d4bd02e2629a4bc130525da4a7db75ebc3aa121
Jeff Cogswell <jeffcogs@gmail.com>
2022-11-17 12:29:49 -0500

    Drink List component part 1

--- BeveragesFront/src/app/drink-list/drink-list.component.ts
+++ BeveragesFront/src/app/drink-list/drink-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Drink } from '../drink';
+import { DrinkService } from '../drink.service';
 
 @Component({
-  selector: 'app-drink-list',
-  templateUrl: './drink-list.component.html',
-  styleUrls: ['./drink-list.component.css']
+	selector: 'app-drink-list',
+	templateUrl: './drink-list.component.html',
+	styleUrls: ['./drink-list.component.css']
 })
 export class DrinkListComponent implements OnInit {
 
-  constructor() { }
+	TheList: Drink[] = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private DrnSrv: DrinkService) { }
+
+	ngOnInit(): void {
+		this.refresh();
+	}
+
+	refresh() {
+		this.DrnSrv.getAll(
+			(result: Drink[]) => {
+				this.TheList = result;
+			}
+		);
+	}
+
+	deleteOne(id: number) {
+
+	}
+
+	updateOne(drink: Drink) {
+
+	}
 
 }
