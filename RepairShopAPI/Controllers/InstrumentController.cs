f0592b9a61f1d863137ca90be13f009c7a152e8f
Jeff Cogswell <jeffcogs@gmail.com>
2022-11-21 13:34:35 -0500

    Instrument controller

---
+++ RepairShopAPI/Controllers/InstrumentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepairShopAPI.Controllers
+{
+	[Route("instrument")]
+	[ApiController]
+	public class InstrumentController : ControllerBase
+	{
+		[HttpGet]
+		public List<Instrument> GetAll()
+		{
+			return Instrument.GetAll();
+		}
+
+		[HttpGet("{id}")]
+		public Instrument GetOne(int id)
+		{
+			return Instrument.GetOne(id);
+		}
+
+		[HttpPost]
+		public Instrument Add(Instrument inst)
+		{
+			return Instrument.Add(inst);
+		}
+
+		[HttpDelete]
+		public void Delete(int id)
+		{
+			Instrument.Delete(id);
+		}
+
+		[HttpPut]
+		public void Update(Instrument inst)
+		{
+			Instrument.Update(inst);
+		}
+
+
+	}
+}
